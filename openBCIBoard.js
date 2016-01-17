daa76bcff1c5b51bb9cf87ae54735072ad3acfd3
AJ Keller <pushtheworldllc@gmail.com>
2016-01-17 15:30:19 -0500

    Cause Craft

--- openBCIBoard.js
+++ openBCIBoard.js
@@ -4,8 +4,8 @@ var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 var stream = require('stream');
 var serialPort = require('serialport');
-var openBCISample = require('./openBCISample' || __dirname+ '/../node_modules/openbci-sdk/OpenBCISample');
-var k = require('./openBCIConstants' || __dirname+ '/../node_modules/openbci-sdk/OpenBCIConstants');
+var openBCISample = require('./openBCISample' || __dirname+ '/../node_modules/openbci-sdk/openBCISample');
+var k = require('./openBCIConstants' || __dirname+ '/../node_modules/openbci-sdk/openBCIConstants');
 
 
 function OpenBCIFactory() {
